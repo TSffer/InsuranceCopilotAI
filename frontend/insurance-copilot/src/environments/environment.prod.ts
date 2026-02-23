5d1369839fa32ace330ff0049f5a3d097cd4d1a3
Luis Fernando Ttito <luis.ttito@ucsp.edu.pe>
2026-02-23 00:55:40 -0500

    v0.9.3

--- frontend/insurance-copilot/src/environments/environment.prod.ts
+++ frontend/insurance-copilot/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ca-backend.internal.livelyglacier-ed8fba0b.eastus2.azurecontainerapps.io/api/v1',
+  apiUrl: 'https://ca-backend.livelyglacier-ed8fba0b.eastus2.azurecontainerapps.io/api/v1',
 };
